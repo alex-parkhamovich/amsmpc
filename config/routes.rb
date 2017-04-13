9402ed5c539cd4aef6fda4a6fcf37ee5b7af5e67
Alexander <lml.alexander.lml@gmail.com>
2017-04-13 14:15:46 +0300

    AMS-2 user main info instances, doctor, patient, super_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :patient_users
+  devise_for :doctor_users
+  devise_for :super_users
+  root 'home#index'
 end
